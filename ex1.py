20ab0005be2b1562a2ef7ac7df5fa6a5ca2a3508
Stefan <andreivacaru53@gmail.com>
2026-03-09 18:42:26 +0200

    yes

---
+++ ex1.py
@@ -0,0 +1,11 @@
+import math
+u=1.0
+u_salvat=1.0
+p10=0
+while(1.0+u)!=1.0:
+    u_salvat=u
+    p10+=1
+    u=10**(-p10)
+print(u)
+print(f"Cel mai mic m: {p10- 1}")
+print(f"Valoarea u: 10^-{p10 - 1}")
